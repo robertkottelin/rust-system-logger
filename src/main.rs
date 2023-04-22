9e0f514e88e6423dd27467684b759c6f39416fdb
robertkottelin <robert.kottelin@gmail.com>
2023-04-22 22:26:34 +0200

    working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{fs::File, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // read system logs on ubuntu
+        let mut file = File::open("/var/log/syslog").unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        println!("With text:\n{}", contents);
+
+        // sleep for 1 second
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+
 }
